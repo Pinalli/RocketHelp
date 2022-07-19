582fae42e94e95fe5c05c65925f12e805b743318
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-19 09:41:47 -0300

    changed location folder

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { Button as ButtonNativeBase, IButtonProps, Heading} from 'native-base';
+type Props = IButtonProps &{
+    title: string;
+}
+
+
+export function Button({title, ...res}: Props) {
+  return (
+    <ButtonNativeBase
+    bg="green.700" 
+    h={14}
+    fontSize ="sm"
+    rounded="sm"
+    _pressed={{bg:"green.400"}}
+    {...res}
+    >
+        <Heading color="white" fontSize="sm">
+            {title}
+    
+        </Heading>
+    </ButtonNativeBase>
+  );
+}
