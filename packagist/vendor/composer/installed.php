b9b830c71f4fd724c6215701c4ff2c6ece3f2e22
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-18 21:57:42 -0300

    screen and components

---
+++ packagist/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'betop/packagist',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'betop/digital-cep-php-composer' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ac62d99b17b606b585b4314ca97d6c1497d200d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../betop/digital-cep-php-composer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'betop/packagist' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
