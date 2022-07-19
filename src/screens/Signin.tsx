43d017f5d26b131d67d6425ce194b7a6277d40f1
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-19 19:00:23 -0300

    react navigation native

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -21,7 +21,7 @@ export function SignIn(){
         <VStack flex={1} alignItems="center" bg="gray.800" px={8} pt={24}>
             <Logo/>
         < Heading color="gray.100"  fontSize="xl" mt={20} mb={6}>
-        Acesse sua conta   
+        Access your account
         </Heading>
 
         <Input placeholder="E-mail" 
