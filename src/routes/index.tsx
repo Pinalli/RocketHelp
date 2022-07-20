dd2784d74813680b10c1101549a84385f9df46bd
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-20 19:02:44 -0300

    authentication

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,35 @@
+import React, {useState, useEffect} from 'react'
 import {NavigationContainer} from '@react-navigation/native';
-
-import {SignIn} from '../screens/signin';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import { SignIn } from '../screens/SignIn';
 import { AppRoutes } from './app.routes';
+import { Loading } from '../components/Loading';
+
+
+
 
 export function Routes(){
+    const [loading, setIsLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+    useEffect(() => {
+        const subscriber = auth()
+        .onAuthStateChanged(response => {
+            setUser(response);
+            setIsLoading(false);
+        });
+
+        return subscriber;
+
+    },[]);
+
+    if(loading){
+        return <Loading/>;
+    }
+
     return (
         <NavigationContainer>
-            <AppRoutes />
+            {user ? <AppRoutes/> : <SignIn />}
         </NavigationContainer>
-    );
+    )
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import {Alert} from 'react-native';
 import {VStack, Heading, Icon, useTheme} from 'native-base';
 import {Envelope, Key} from 'phosphor-react-native';
 import Logo from '../assets/logo_primary.svg';
@@ -7,15 +9,40 @@ import {Button} from '../components/Button';
 
 
 export function SignIn(){
-   
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-   
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');   
     const {colors} = useTheme();
 
     function handleSignIn(){
-       
-    }
+        if(!email || !password){
+           return  Alert.alert('Log in', 'Please fill all fields');                  
+        }      
+        setIsLoading(true);
+        auth().
+        signInWithEmailAndPassword(email, password)
+        .catch(error => {
+            console.log(error);
+            setIsLoading(false);
+           
+            if(error.code === 'auth/invalid-email'){
+               return  Alert.alert('Log in', 'Invalid email');
+            }
+            if(error.code === 'auth/wrong-password'){
+                return  Alert.alert('Log in', 
+                'wrong password or email');
+             }
+
+            if(error.code === 'auth/user-not found'){
+                return  Alert.alert('Log in', 
+                'unregistered user');
+             }
+              return Alert.alert('Log in', 'Something went wrong');
+
+        });   
+    }  
+        
 
     return (
         <VStack flex={1} alignItems="center" bg="gray.800" px={8} pt={24}>
@@ -27,17 +54,19 @@ export function SignIn(){
         <Input placeholder="E-mail" 
         mb={4}
         InputLeftElement = {<Icon as={<Envelope color={colors.gray[300]}/>}ml={4} />}
-        onChangeText={setName}
+        onChangeText={setEmail}
         /> 
-        <Input placeholder="Senha" 
+        <Input placeholder="Password" 
          mb={8}
          InputLeftElement = {<Icon as={<Key color={colors.gray[300]}/>} ml={4}/>}
          secureTextEntry
          onChangeText={setPassword}
         />
         
-        <Button title='Entrar' w="full" mt={4}
-        
+        <Button title='Log in' 
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
         />
         </VStack>
     );
